5318248ae28c3e728093c9f34388737126051f29
Adillah Putri <adllhputri@gmail.com>
2024-10-27 16:43:01 +0700

    Modify checker function

--- renderer/index.js
+++ renderer/index.js
@@ -656,30 +656,35 @@ const checker = () => {
     log.transports.file.resolvePath = () => `out/${logID}.log`
     log.info("Inserting test for " + currentID)
     log.info("logID is " + logID)
-    const msLeft = Date.parse("2022-11-09T22:56:00.000+07:00") - Date.now()
-    // setTimeout(scenarioOne, msLeft)
-    // setTimeout(() => {
-    //   codemirrorView.dispatch({
-    //     changes: {
-    //       from: 0,
-    //       to: codemirrorView.state.doc.length,
-    //       insert: scenarioTwoCode
-    //     },
-    //   })
-    // }, 3 * SECOND)
-    // setTimeout(scenarioTwo, msLeft)
-    // const randomDelay = randInt(1000)
-    // setTimeout(scenarioThree, msLeft + randomDelay)
-    // setTimeout(() => {
-    //   codemirrorView.dispatch({
-    //     changes: {
-    //       from: 0,
-    //       to: codemirrorView.state.doc.length,
-    //       insert: scenarioFourCode
-    //     },
-    //   })
-    // }, 3 * SECOND)
-    // setTimeout(scenarioFour, msLeft)
+    const msLeft = Date.parse("2022-11-04T14:00:00.000+07:00") - Date.now()
+    if (currentTestScenario === 1){
+      setTimeout(scenarioOne, msLeft)
+    } else if (currentTestScenario === 2) {
+      setTimeout(() => {
+        codemirrorView.dispatch({
+          changes: {
+            from: 0,
+            to: codemirrorView.state.doc.length,
+            insert: scenarioTwoCode
+          },
+        })
+      }, 3 * SECOND)
+      setTimeout(scenarioTwo, msLeft)
+    } else if (currentTestScenario === 3) {
+      const randomDelay = randInt(1000)
+      setTimeout(scenarioThree, msLeft + randomDelay)
+    } else {
+      setTimeout(() => {
+        codemirrorView.dispatch({
+          changes: {
+            from: 0,
+            to: codemirrorView.state.doc.length,
+            insert: scenarioFourCode
+          },
+        })
+      }, 3 * SECOND)
+      setTimeout(scenarioFour, msLeft)
+    }
   } else {
     setTimeout(checker, SECOND)
   }
